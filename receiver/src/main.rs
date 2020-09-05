2c29437594aabe0c150bc57faea49cc784718233
ftilde <ftilde@tamepointer.de>
2020-09-05 18:15:14 +0200

    Remove intermediate buffer in receiver

--- receiver/src/main.rs
+++ receiver/src/main.rs
@@ -5,16 +5,12 @@ use cpal::{
     SampleFormat,
 };
 use std::net::UdpSocket;
-use std::sync::atomic::Ordering;
-use std::sync::Arc;
 
 type Sample = i16;
 const MAX_SAMPLES_PER_PACKET: usize = 512;
 const SINGLE_SAMPLE_SIZE: usize = std::mem::size_of::<Sample>();
 const MAX_PACKET_SIZE: usize = MAX_SAMPLES_PER_PACKET * SINGLE_SAMPLE_SIZE;
 const SAMPLE_RATE: usize = 48000;
-
-const RING_BUFFER_SIZE: usize = SAMPLE_RATE / 8;
 
 #[repr(C, align(2))] //Depends on Sample!!
 struct PacketBuffer([u8; MAX_PACKET_SIZE]);
@@ -33,50 +29,59 @@ fn main() {
         .expect("no supported format?!")
         .with_sample_rate(cpal::SampleRate(SAMPLE_RATE as _));
 
-    // Safety: Just plain old data
-    let sample_buffer: [std::sync::atomic::AtomicI16; RING_BUFFER_SIZE] =
-        unsafe { std::mem::zeroed() };
-
-    let sample_source = Arc::new(sample_buffer);
-    let mut source_pos = 0;
-    let sample_sink = Arc::clone(&sample_source);
-    let mut sink_pos = RING_BUFFER_SIZE / 2;
-
     let output_config = output_format.into();
+
+    let port = 13337;
+    let socket = UdpSocket::bind(("0.0.0.0", port)).unwrap();
+    socket.set_nonblocking(true).unwrap();
+
+    let mut recv_buf = unsafe { std::mem::zeroed::<PacketBuffer>() };
+    let mut recv_data_pos = 0;
+    let mut num_received = 0;
     let stream = output_device
         .build_output_stream(
             &output_config,
             move |data: &mut [Sample], _info| {
-                for o in data {
-                    *o = sample_source[source_pos].load(Ordering::SeqCst);
-                    source_pos = (source_pos + 1) % RING_BUFFER_SIZE;
+                let mut data_pos = 0;
+                loop {
+                    let recv_data = &recv_buf.0[..].as_slice_of::<Sample>().unwrap();
+                    while recv_data_pos < num_received {
+                        if data_pos == data.len() {
+                            return;
+                        }
+
+                        data[data_pos] = recv_data[recv_data_pos];
+
+                        data_pos += 1;
+                        recv_data_pos += 1;
+                    }
+                    recv_data_pos = 0;
+                    num_received = 0;
+
+                    let n = match socket.recv(&mut recv_buf.0) {
+                        Ok(n) => n,
+                        Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                            for i in data_pos..data.len() {
+                                data[i] = 0;
+                            }
+                            eprintln!("No Packet: Underrun!");
+                            return;
+                        }
+                        Err(e) => panic!("Recv error: {}", e),
+                    };
+                    assert!(
+                        n % SINGLE_SAMPLE_SIZE == 0,
+                        "Packet contains incomplete sample"
+                    );
+                    num_received = n / SINGLE_SAMPLE_SIZE;
                 }
             },
             |e| panic!("Error! {:?}", e),
         )
         .unwrap();
 
-    let port = 13337;
-    let socket = UdpSocket::bind(("0.0.0.0", port)).unwrap();
-    //socket.set_read_timeout(Some(Duration::new(5, 0))).unwrap();
-
-    // Safety: Just u8s. Zeros are fine.
-    let mut buf = unsafe { std::mem::zeroed::<PacketBuffer>() };
-
     stream.play().unwrap();
     loop {
-        let n = socket.recv(&mut buf.0).unwrap();
-        assert!(
-            n % SINGLE_SAMPLE_SIZE == 0,
-            "Packet contains incomplete sample"
-        );
-
-        let data = &buf.0[..n].as_slice_of::<Sample>().unwrap();
-        //println!("Got {} samples", data.len());
-
-        for s in data.iter() {
-            sample_sink[sink_pos].store(*s, Ordering::SeqCst);
-            sink_pos = (sink_pos + 1) % RING_BUFFER_SIZE;
-        }
+        std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
